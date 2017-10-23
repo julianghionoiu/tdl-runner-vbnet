1a1ab8ef88f8195a56a22ead3917f61f5f4c350a
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2017-10-23 20:50:03 +0300

    Add start description

--- src/BeFaster.App/Start.vb
+++ src/BeFaster.App/Start.vb
@@ -2,6 +2,40 @@
 Imports BeFaster.Runner
 Imports BeFaster.Runner.Extensions
 
+'~~~~~~~~~~ Running the system: ~~~~~~~~~~~~~
+
+'  From IDE, run without args:
+'     Set the value of the `WithActionIfNoArgs` below
+'     Run this project from the IDE.
+'
+'  From IDE, run with args:
+'     Create a separate Run configuration
+'     Add the name of the action as an argument to the command-line 
+'     Run the newly created configuration from the IDE.
+'
+'  Available actions:
+'       * getNewRoundDescription    - Get the round description (call once per round).
+'       * testConnectivity          - Test you can connect to the server (call any number of time)
+'       * deployToProduction        - Release your code. Real requests will be used to test your solution.
+'                                     If your solution is wrong you get a penalty of 10 minutes.
+'                                     After you fix the problem, you should deploy a new version into production.
+'
+'  ~~~~~~~~~~ The workflow ~~~~~~~~~~~~~
+'
+'  +------+-----------------------------------------+-----------------------------------------------+
+'  | Step |          IDE                            |         Web console                           |
+'  +------+-----------------------------------------+-----------------------------------------------+
+'  |  1.  |                                         | Start a challenge, should display "Started"   |
+'  |  2.  | Run "getNewRoundDescription"            |                                               |
+'  |  3.  | Read description from ./challenges      |                                               |
+'  |  4.  | Implement the required method in        |                                               |
+'  |      |   ./src/BeFaster.App/Solutions          |                                               |
+'  |  5.  | Run "testConnectivity", observe output  |                                               |
+'  |  6.  | If ready, run "deployToProduction"      |                                               |
+'  |  7.  |                                         | Type "done"                                   |
+'  |  8.  |                                         | Check failed requests                         |
+'  |  9.  |                                         | Go to step 2.                                 |
+'  +------+-----------------------------------------+-----------------------------------------------+
 Module Start
 
     Sub Main(args As String())
