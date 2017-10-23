e890c4d90e68b702bd7b32a7dd509f4c716abc31
Egor Raskhodchikov <raskhodchikov.egor@gmail.com>
2017-10-23 20:46:49 +0300

    Add sample test

--- src/BeFaster.App.Tests/SumSolutionTest.vb
+++ src/BeFaster.App.Tests/SumSolutionTest.vb
@@ -1,3 +1,12 @@
-﻿Public Class SumSolutionTest
+﻿Imports BeFaster.App.Solutions
+Imports NUnit.Framework
+
+<TestFixture>
+Public Class SumSolutionTest
+
+    <TestCase(1, 1, ExpectedResult := 2)>
+    Public Function ComputeSum(x As Int32, y As Int32)
+        Return Sum.Sum(x, y)
+    End Function
 
 End Class
